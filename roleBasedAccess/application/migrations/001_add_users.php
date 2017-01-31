d4e52079c66ba6e70105286b524d53bd1a158957
fatih <fatih@fatih-K55VJ>
2017-01-31 20:27:40 +0300

    Revert "Fatih-K55VJ üzerine proje kuruldu "
    This reverts commit e8e5995b6cdce0544d4262a79b5f558d47e17ea4.

--- roleBasedAccess/application/migrations/001_add_users.php
+++ roleBasedAccess/application/migrations/001_add_users.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Migration_Add_users extends CI_Migration {
+class Migration_Add_blog extends CI_Migration {
 
     public function up() {
         $this->dbforge->add_field(array(
